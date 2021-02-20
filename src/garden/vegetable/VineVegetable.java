9abb8556292ba7e102dbd7f5872b4858d238e1cf
Jason <JasonWHowse@gmail.com>
2021-02-20 17:05:56 -0600

    S01.8. Packages and Imports

---
+++ src/garden/vegetable/VineVegetable.java
@@ -0,0 +1,13 @@
+/*
+Learn Programming Academy's Java 1Z0-815 Certification Exam Course
+Section 2: Create an class in a package
+ */
+
+package garden.vegetable;
+
+public class VineVegetable {
+    // Main Method simply outputs some data
+    public static void main(String[] args) {
+        System.out.println("I am a vegetable that grows on a vine.");
+    }//public static void main(String[] args) {
+}//public class VineVegetable {
