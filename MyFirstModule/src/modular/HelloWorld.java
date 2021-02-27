60d9e3c6b33869b7cf461b0053eb30428d07800a
Jason <JasonWHowse@gmail.com>
2021-02-26 19:54:52 -0600

    S12.124. Creating, Compiling and Running Modules

---
+++ MyFirstModule/src/modular/HelloWorld.java
@@ -0,0 +1,14 @@
+/*
+Learn Programming Academy's Java 1Z0-815 Certification Exam Course
+Section 12: Understanding Modules
+Topic: Describe how a modular project is compiled and run.
+*/
+
+package modular;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello new modular world");
+    }//public static void main(String[] args) {
+}//public class HelloWorld {
+
